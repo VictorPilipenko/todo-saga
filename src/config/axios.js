2b3ed5ad78f79c74276cb4d68f5263456b916f15
thejustvic <thejustvic@gmail.com>
2020-11-18 18:24:52 +0200

    NProgress with axios

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,36 @@
 import axios from "axios";
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 const instance = axios.create({
   baseURL: 'http://localhost:3000/',
 });
+
+const calculatePercentage = (loaded, total) => Math.floor(loaded * 1.0) / total
+
+instance.interceptors.request.use(
+  config => {
+    NProgress.start()
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
+
+instance.defaults.onDownloadProgress = e => {
+  const percentage = calculatePercentage(e.loaded, e.total)
+  NProgress.set(percentage)
+}
+
+instance.interceptors.response.use(
+  response => {
+    NProgress.done()
+    return response;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
 
 export default instance;
---
+++ src/config/nprogress.js
@@ -0,0 +1,9 @@
+const config = {
+  showSpinner: false,
+  minimum: 0.2,
+  easeing: 'swing',
+  speed: 1000,
+  trickleRate: 0.2
+}
+
+export default config
