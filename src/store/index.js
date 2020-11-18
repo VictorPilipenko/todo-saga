a71aab5bbf82da86a888de5baa9eff0202222aea
thejustvic <thejustvic@gmail.com>
2020-11-18 18:28:32 +0200

    disable react-dev-tools for production

--- src/store/index.js
+++ src/store/index.js
@@ -24,4 +24,13 @@ function* rootSaga() {
 const store = createStore(reducer, enhancer);
 sagaMiddleware.run(rootSaga);
 
+// disable react-dev-tools for production
+if (process.env.NODE_ENV === 'production') {
+  if (typeof window.__REACT_DEVTOOLS_GLOBAL_HOOK__ === "object") {
+    for (let [key, value] of Object.entries(window.__REACT_DEVTOOLS_GLOBAL_HOOK__)) {
+      window.__REACT_DEVTOOLS_GLOBAL_HOOK__[key] = typeof value == "function" ? () => { } : null;
+    }
+  }
+}
+
 export default store;
