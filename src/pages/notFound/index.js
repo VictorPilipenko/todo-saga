54ee7b2b70c94d2d26f78cb27e3066524103a6e8
thejustvic <thejustvic@gmail.com>
2020-11-18 18:25:21 +0200

    improve notFound page

--- src/pages/notFound/index.js
+++ src/pages/notFound/index.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import { Helmet } from "react-helmet";
+import { useHistory } from 'react-router-dom';
+import { Button } from '../../common/button';
 import { Wrapper } from "./index.styled";
 
-const pageTitle = "todo - About us."
+const pageTitle = "todo - Page Not Found"
 const pageDescription = "We love learning - every day, in every form."
 
-const NotFound = () => <>
-  <Helmet>
-    <title>{pageTitle}</title>
-    <meta property="og:title" content={pageTitle} />
-    <meta name="description" content={pageDescription} />
-    <meta name="twitter:title" content={pageTitle} />
-    <meta name="twitter:description" content={pageDescription} />
-  </Helmet>
-  <Wrapper>404!</Wrapper>
-</>
+const NotFound = () => {
+  const history = useHistory()
+  return <>
+    <Helmet>
+      <title>{pageTitle}</title>
+      <meta property="og:title" content={pageTitle} />
+      <meta name="description" content={pageDescription} />
+      <meta name="twitter:title" content={pageTitle} />
+      <meta name="twitter:description" content={pageDescription} />
+    </Helmet>
+    <Wrapper>404!</Wrapper>
+    <Button
+      onClick={() => {
+        history.push('/todo')
+      }}
+    >Go Home</Button>
+  </>
+}
 
 export default NotFound;
 
--- src/pages/notFound/index.styled.js
+++ src/pages/notFound/index.styled.js
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-  background: linear-gradient(84deg, #E62D24, #FD8F21);
+  background: linear-gradient(84deg, #a8ff61, #38d9a9);
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
-  color: #FD8F21;
+  color: #38d9a9;
   font-size: 140px;
   position: fixed;
   top: 50%;
