a78a3aaed8023b1c61cbe5813d41f42c348ad4dd
thejustvic <thejustvic@gmail.com>
2020-11-27 15:43:16 +0200

    useInput hook

---
+++ src/common/search/SearchBox.js
@@ -0,0 +1,35 @@
+import React, { useCallback } from 'react';
+import PropTypes from 'prop-types';
+import { Input } from 'antd';
+import { useDispatch } from 'react-redux';
+import useInput from '../../hooks/useInput';
+
+const { Search } = Input;
+
+const SearchBox = ({ placeholder, action, loading }) => {
+  const dispatch = useDispatch();
+  const [searchKeyword, setSearchKeyword, changeSearchKeyword] = useInput('');
+  const onSearch = useCallback(() => {
+    dispatch(action(searchKeyword));
+  }, [searchKeyword]);
+  return (
+    <div>
+      <Search
+        placeholder={placeholder}
+        value={searchKeyword}
+        onChange={changeSearchKeyword}
+        onSearch={onSearch}
+        style={{ height: '30px' }}
+        loading={loading}
+      />
+    </div>
+  );
+};
+
+SearchBox.propTypes = {
+  placeholder: PropTypes.string,
+  action: PropTypes.func.isRequired,
+  loading: PropTypes.bool,
+};
+
+export default SearchBox;
---
+++ src/hooks/useInput.js
@@ -0,0 +1,11 @@
+import { useState, useCallback } from 'react';
+
+const useInput = (init) => {
+  const [value, setValue] = useState(init);
+  const changeValue = useCallback((e) => {
+    setValue(e.target.value);
+  }, []);
+  return [value, setValue, changeValue];
+};
+
+export default useInput;
