9238f09886e435a66b77b0c7f6cb15aa616d682c
thejustvic <thejustvic@gmail.com>
2020-11-30 20:10:02 +0200

    configure connected-react-router

--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,28 @@
 
 import createSagaMiddleware from "redux-saga";
 import { createStore, applyMiddleware, compose } from "redux";
+import { connectRouter, routerMiddleware } from 'connected-react-router'
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { combineReducers } from "redux";
 import { all } from "redux-saga/effects";
 import todos from "../store/reducers/todo";
 import todosSaga from '../store/watchers/todo'
 
+import { createBrowserHistory } from "history"
+export const history = createBrowserHistory()
+
 const sagaMiddleware = createSagaMiddleware();
-const middlewares = [sagaMiddleware];
+const middlewares = [
+  sagaMiddleware,
+  routerMiddleware(history)
+]
 const enhancer =
   process.env.NODE_ENV === 'production'
     ? compose(applyMiddleware(...middlewares))
     : composeWithDevTools(applyMiddleware(...middlewares));
 const reducer = combineReducers({
-  todos
+  todos,
+  router: connectRouter(history),
 })
 function* rootSaga() {
   yield all([
