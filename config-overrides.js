692498a10e1912b9e3815bf7b08433dc978d7076
thejustvic <thejustvic@gmail.com>
2020-12-12 10:21:01 +0200

    refactor config

--- config-overrides.js
+++ config-overrides.js
@@ -5,15 +5,15 @@ const AntDesignThemePlugin = require("antd-theme-webpack-plugin");
 const { getLessVars } = require('antd-theme-generator');
 const themeVariables = getLessVars(path.join(__dirname, './src/themes/vars.less'))
 
-// const defaultVars = getLessVars('./node_modules/antd/lib/style/themes/default.less')
+const defaultVars = getLessVars('./node_modules/antd/lib/style/themes/default.less')
 
 const darkVars = {
   ...getLessVars('./node_modules/antd/lib/style/themes/dark.less'),
-  // '@primary-color': defaultVars['@primary-color'], '@picker-basic-cell-active-with-range-color': 'darken(@primary-color, 20%)'
+  '@primary-color': defaultVars['@primary-color'], '@picker-basic-cell-active-with-range-color': 'darken(@primary-color, 20%)'
 };
 const lightVars = {
   ...getLessVars('./node_modules/antd/lib/style/themes/compact.less'),
-  // '@primary-color': defaultVars['@primary-color']
+  '@primary-color': defaultVars['@primary-color']
 };
 
 fs.writeFileSync('./src/themes/dark.json', JSON.stringify(darkVars));
@@ -24,12 +24,13 @@ const options = {
   stylesDir: path.join(__dirname, './src'),
   antDir: path.join(__dirname, './node_modules/antd'),
   varFile: path.join(__dirname, './src/themes/vars.less'),
+  lessUrl: "https://cdnjs.cloudflare.com/ajax/libs/less.js/2.7.2/less.min.js",
   themeVariables: Array.from(new Set([
     ...Object.keys(darkVars),
     ...Object.keys(lightVars),
     ...Object.keys(themeVariables),
   ])),
-  generateOnce: false,
+  generateOnce: true,
 }
 
 module.exports = override(
