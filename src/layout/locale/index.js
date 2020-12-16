177262f15f887d92a7de149c90c83f96c6956c12
thejustvic <thejustvic@gmail.com>
2020-12-16 21:12:23 +0200

    refactor LocaleSwitcher

--- src/layout/locale/index.js
+++ src/layout/locale/index.js
@@ -1,35 +1,23 @@
 
 import { Select } from 'antd'
 import { useDispatch, useSelector } from 'react-redux'
-import { appLocale } from '../../store/actions/locale'
-
-import RU_Components from 'antd/es/locale/ru_RU'
-import US_Components from 'antd/es/locale/en_US'
-
-import RU from '../../locale/ru'
-import EN from '../../locale/en'
+import { appLocale } from '../../store/actions/local/locale'
 import { FormattedMessage } from 'react-intl'
-
+import { getComponents, getMessages } from '../../utils/locale'
 
 const LocaleSwitcher = () => {
   const dispatch = useDispatch()
-  const { locale } = useSelector(state => state.locale)
+  const { language } = useSelector(state => state.locale)
 
   return (
     <Select
-      placeholder="Please select locale"
-      value={locale}
-      onSelect={value => {
-        value === 'en' && dispatch(appLocale({
-          locale: 'en',
-          messages: EN,
-          components: US_Components,
-        }))
-        value === 'ru' && dispatch(appLocale({
-          locale: 'ru',
-          messages: RU,
-          components: RU_Components,
-        }))
+      value={language}
+      onSelect={language => {
+        dispatch(appLocale({
+          language,
+          messages: getMessages(language),
+          components: getComponents(language),
+        })) 
       }}
     >
       <Select.Option value="en"><FormattedMessage id="locale.en" /></Select.Option>
