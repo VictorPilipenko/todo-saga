74c743b6cc2175fe7fd7b2d616f3a4c7cd76e5cd
thejustvic <thejustvic@gmail.com>
2020-12-16 20:56:27 +0200

    locale util

---
+++ src/utils/locale.js
@@ -0,0 +1,27 @@
+import RU_Components from 'antd/es/locale/ru_RU'
+import US_Components from 'antd/es/locale/en_US'
+
+import RU from '../locale/ru'
+import US from '../locale/en'
+
+const getMessages = (language) => {
+  switch (language) {
+    case 'ru':
+      return RU
+    default:
+      return US
+  }
+}
+const getComponents = (language) => {
+  switch (language) {
+    case 'ru':
+      return RU_Components
+    default:
+      return US_Components
+  }
+}
+
+export {
+  getMessages,
+  getComponents
+}
