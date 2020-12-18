7fec2b467acade10cac642dd1bede9d879206da7
thejustvic <thejustvic@gmail.com>
2020-12-18 21:08:19 +0200

    refactor global styles

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,8 +1,6 @@
 import { createGlobalStyle } from "styled-components";
-import { normalize } from "styled-normalize";
 
 const GlobalStyle = createGlobalStyle`
-  ${normalize};
 
   #nprogress .bar {
     height: 4px;
@@ -11,6 +9,7 @@ const GlobalStyle = createGlobalStyle`
   .ant-form-vertical .ant-form-item-label {
     padding: 0px !important;
   }
+  
 `;
 
 export default GlobalStyle;
