ca879639ea9d2c3eaf7790aff56654f2e84f3363
thejustvic <thejustvic@gmail.com>
2020-12-21 22:48:48 +0200

    add separate condition to useDispatchOnFirstMount hook

--- src/hooks/useDispatchOnFirstMount.js
+++ src/hooks/useDispatchOnFirstMount.js
@@ -2,17 +2,18 @@ import { useEffect } from "react"
 import { useDispatch } from "react-redux"
 import { useIsFirstMount } from "./useIsFirstMount"
 
-const useDispatchOnFirstMount = ({ handler }) => {
+const useDispatchOnFirstMount = ({ handler, condition = true }) => {
   const dispatch = useDispatch()
   const isFirstMount = useIsFirstMount()
   useEffect(() => {
-    if (isFirstMount) {
+    if (isFirstMount && condition) {
       dispatch(handler)
     }
   }, [
     dispatch,
     isFirstMount,
     handler,
+    condition
   ])
 }
 
