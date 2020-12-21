59d46f88d2ac6c31621982917143ae07baa95dff
thejustvic <thejustvic@gmail.com>
2020-12-21 22:45:57 +0200

    get profile route

--- server.js
+++ server.js
@@ -1,14 +1,34 @@
+const cors = require('cors')
 const jsonServer = require('json-server')
 const auth = require('json-server-auth')
 const socket = require('socket.io')
- 
 const middlewares = jsonServer.defaults()
 const app = jsonServer.create()
 const router = jsonServer.router('db.json')
- 
+const jwt = require('jsonwebtoken')
+app.use(cors())
+
+app.get('/profile', auth, (req, res, next) => {
+  const token = req.header('Authorization') ? req.header('Authorization').replace('Bearer ', '') : null;
+  if (token) {
+    try {
+      const data = jwt.verify(token, 'json-server-auth-123456')
+      const { db } = req.app;
+      let user = db.get('users').find({ email: data.email }).value();
+      res.json(user)
+    }
+    catch (error) {
+      res.status(500).json(error)
+    }
+
+  } else {
+    res.status(404).json("User not authorized")
+  }
+});
+
 // /!\ Bind the router db to the app
 app.db = router.db
- 
+
 // You must apply the auth middleware before the router
 app.use(middlewares)
 app.use(auth)
