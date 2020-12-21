27ae59ddcc4129d52690ca10010f3fb16f229e17
thejustvic <thejustvic@gmail.com>
2020-12-21 22:45:33 +0200

    theme update

--- config-overrides.js
+++ config-overrides.js
@@ -3,28 +3,35 @@ const fs = require("fs");
 const { override, fixBabelImports, addLessLoader, addWebpackPlugin } = require('customize-cra');
 const AntDesignThemePlugin = require("antd-theme-webpack-plugin");
 const { getLessVars } = require('antd-theme-generator');
-const themeVariables = getLessVars(path.join(__dirname, './src/themes/vars.less'))
+
+const lightThemeVariables = getLessVars(path.join(__dirname, './src/styles/light.less'))
+const darkThemeVariables = getLessVars(path.join(__dirname, './src/styles/dark.less'))
 
 const darkVars = {
-  ...getLessVars('./node_modules/antd/lib/style/themes/dark.less')
+  ...getLessVars('./node_modules/antd/lib/style/themes/dark.less'),
+  ...darkThemeVariables
 };
+
 const lightVars = {
-  ...getLessVars('./node_modules/antd/lib/style/themes/compact.less')
+  ...getLessVars('./node_modules/antd/lib/style/themes/compact.less'),
+  ...lightThemeVariables
 };
 
 fs.writeFileSync('./src/themes/dark.json', JSON.stringify(darkVars));
 fs.writeFileSync('./src/themes/light.json', JSON.stringify(lightVars));
-fs.writeFileSync('./src/themes/theme.json', JSON.stringify(themeVariables));
+fs.writeFileSync('./src/themes/light-overrides.json', JSON.stringify(lightThemeVariables));
+fs.writeFileSync('./src/themes/dark-overrides.json', JSON.stringify(darkThemeVariables));
 
 const options = {
   stylesDir: path.join(__dirname, './src'),
   antDir: path.join(__dirname, './node_modules/antd'),
-  varFile: path.join(__dirname, './src/themes/vars.less'),
+  varFile: path.join(__dirname, './src/styles/light.less'),
   lessUrl: "https://cdnjs.cloudflare.com/ajax/libs/less.js/2.7.3/less.min.js",
   themeVariables: Array.from(new Set([
     ...Object.keys(darkVars),
     ...Object.keys(lightVars),
-    ...Object.keys(themeVariables),
+    ...Object.keys(lightThemeVariables),
+    ...Object.keys(darkThemeVariables),
   ])),
   generateOnce: true,
 }
@@ -42,3 +49,45 @@ module.exports = override(
     }
   })
 );
+
+
+
+// '@border-radius-base': '4px;',
+// '@border-color-base': '#919191;',
+// '@border-color-split': '#919191;',
+// '@border-color-inverse': '#919191;',
+
+// '@primary-color': '#32475c;',
+// '@link-color': '#1890ff;',
+
+// '@success-color': '#52c41a;',
+// '@warning-color': '#faad14;',
+// '@error-color': '#f5222d;',
+
+// '@font-size-base': '16px;',
+// '@heading-color': 'rgba(0, 0, 0, 0.85);',
+
+// '@text-color': 'rgba(0, 0, 0, 0.65);',
+// '@text-color-secondary': 'rgba(0, 0, 0, 0.45);',
+
+// '@disabled-color': 'rgba(0, 0, 0, 0.25);',
+
+
+
+
+
+
+// @primary-color: #32475c;
+// @link-color: #1890ff;
+
+// @success-color: #52c41a;
+// @warning-color: #faad14;
+// @error-color: #f5222d;
+
+// @font-size-base: 16px;
+// @heading-color: rgba(0, 0, 0, 0.85);
+
+// @text-color: rgba(0, 0, 0, 0.65);
+// @text-color-secondary: rgba(0, 0, 0, 0.45);
+
+// @disabled-color: rgba(0, 0, 0, 0.25);
--- src/styles/global.js
+++ src/styles/global.js
@@ -2,12 +2,20 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
 
-  #nprogress .bar {
-    height: 4px;
-  }
-
   .ant-form-vertical .ant-form-item-label {
     padding: 0px !important;
+  }
+
+  .ant-tag-green {
+    color: #335f1e !important;
+    background: #f6ffed !important;
+    border-color: #b7eb8f !important;
+  }
+
+  .ant-tag-red {
+    color: #f5222d !important;
+    background: #fafafa !important;
+    border-color: #ffa39e !important;
   }
   
 `;
