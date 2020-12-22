ae4acde67e380c3cc682d8169305def889ec7892
thejustvic <thejustvic@gmail.com>
2020-12-22 18:38:26 +0200

    draggable modal

---
+++ src/common/modal/draggable/index.js
@@ -0,0 +1,59 @@
+import Draggable from 'react-draggable'
+import { useState } from 'react'
+import { ModalBlock } from './index.styled'
+
+export const useDraggableModal = () => {
+  const [isVisible, setIsVisible] = useState(false)
+  const [isDisabled, setIsDisabled] = useState(true)
+
+  const showDraggableModal = () => setIsVisible(true)
+  const hideDraggableModal = () => setIsVisible(false)
+
+  const setDisabled = () => setIsDisabled(true)
+  const unsetDisabled = () => setIsDisabled(false)
+
+  const renderDraggableModal = ({
+    render,
+    title,
+    ...props
+  }) => (
+      <ModalBlock
+        destroyOnClose={true}
+        focusTriggerAfterClose={false}
+        visible={isVisible}
+        onCancel={hideDraggableModal}
+        title={
+          <div
+            style={{
+              width: '100%',
+              cursor: 'move',
+            }}
+            onMouseOver={() => {
+              if (isDisabled) {
+                unsetDisabled()
+              }
+            }}
+            onMouseOut={() => {
+              setDisabled()
+            }}
+            // fix eslintjsx-a11y/mouse-events-have-key-events
+            // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/master/docs/rules/mouse-events-have-key-events.md
+            onFocus={() => { }}
+            onBlur={() => { }}
+          >
+            {title ?? 'Draggable Modal'}
+          </div>
+        }
+        modalRender={modal => <Draggable disabled={isDisabled}>{modal}</Draggable>}
+        {...props}
+      >
+        {typeof render === "function" ? render() : <>Empty Modal</>}
+      </ModalBlock>
+    )
+
+  return {
+    showDraggableModal,
+    hideDraggableModal,
+    renderDraggableModal,
+  }
+}
---
+++ src/common/modal/draggable/index.styled.js
@@ -0,0 +1,9 @@
+
+import styled from "styled-components"
+import { Modal } from 'antd'
+
+export const ModalBlock = styled(Modal)`
+  .ant-modal-body {
+    padding-top: 50px;
+  }
+`
