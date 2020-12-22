16fac78d399fca0f810ebbaf059399c1e13c463b
thejustvic <thejustvic@gmail.com>
2020-12-22 18:39:00 +0200

    update sider

--- src/layout/private/sider/index.js
+++ src/layout/private/sider/index.js
@@ -1,40 +1,63 @@
 import { Link, useLocation } from 'react-router-dom'
-import { Menu } from 'antd'
 import {
   HomeOutlined,
-  TeamOutlined,
+  DashboardOutlined,
+  UserOutlined,
 } from '@ant-design/icons'
 import { FormattedMessage } from 'react-intl'
-import { Logo, SiderBlock } from './index.styled'
+import { Logo, MenuBlock, SiderBlock, ItemGroupBlock } from './index.styled'
+import { useDispatch } from 'react-redux';
+import { signOut } from '../../../store/actions/restful/auth'
+import { useModal } from '../../../common/modal/default';
 
 const App = ({ collapsed, onCollapse }) => {
   const location = useLocation()
-
-  return (
+  const dispatch = useDispatch()
+  const { showModal, renderModal } = useModal()
+  return <>
+    {
+      renderModal({
+        onOk: () => dispatch(signOut()),
+        render: () => (
+          <h3><FormattedMessage id="sider.modal.sign.out.title" /></h3>
+        )
+      })
+    }
     <SiderBlock
       collapsible
       collapsed={collapsed}
       onCollapse={onCollapse}
     >
-      <Logo src={`${process.env.PUBLIC_URL}/logo.png`} collapsed={collapsed}/>
-      <Menu
+      <Logo src={`${process.env.PUBLIC_URL}/logo.png`} collapsed={collapsed} />
+      <MenuBlock
         theme="dark"
         mode="inline"
         selectedKeys={[location.pathname]}
       >
-        <Menu.Item key="/todo" danger={true} title={'Home'}>
-          <HomeOutlined />
-          <span><FormattedMessage id="sider.menu.home" /></span>
-          <Link to="/todo"></Link>
-        </Menu.Item>
-        <Menu.Item key="/profile" title={'profile'}>
-          <TeamOutlined />
-          <span><FormattedMessage id="sider.menu.users" /></span>
-          <Link to="/profile"></Link>
-        </Menu.Item>
-      </Menu>
+        <ItemGroupBlock>
+          <MenuBlock.Item key="/todo" danger={true} title={'Home'}>
+            <HomeOutlined />
+            <span><FormattedMessage id="sider.menu.home" /></span>
+            <Link to="/todo"></Link>
+          </MenuBlock.Item>
+          <MenuBlock.Item key="/dashboard" title={'dashboard'}>
+            <DashboardOutlined />
+            <span>dashboard</span>
+            <Link to="/dashboard"></Link>
+          </MenuBlock.Item>
+        </ItemGroupBlock>
+        <MenuBlock.SubMenu key="/profile" icon={<UserOutlined />} title={'profile'}>
+          <MenuBlock.Item key="out" title={'sign out'} onClick={showModal}>
+            <span>out</span>
+          </MenuBlock.Item>
+          <MenuBlock.Item key="/profile" title={'info'}>
+            <span><FormattedMessage id="sider.menu.users" /></span>
+            <Link to="/profile"></Link>
+          </MenuBlock.Item>
+        </MenuBlock.SubMenu>
+      </MenuBlock>
     </SiderBlock>
-  );
-};
+  </>
+}
 
 export default App
--- src/layout/private/sider/index.styled.js
+++ src/layout/private/sider/index.styled.js
@@ -1,4 +1,4 @@
-import { Layout } from 'antd'
+import { Layout, Menu } from 'antd'
 import styled, { css } from "styled-components";
 
 const { Sider } = Layout
@@ -8,6 +8,11 @@ export const SiderBlock = styled(Sider)`
   height: 100vh;
   position: fixed;
   left: 0;
+  
+  .ant-layout-sider-children {
+    display: flex;
+    flex-direction: column;
+  }
 `;
 
 export const Logo = styled.div`
@@ -21,7 +26,22 @@ export const Logo = styled.div`
   `}
   ${({ collapsed }) =>
     collapsed && css`
-      height: 80px;
+      height: 69px;
   `}
   transition: all 0.2s;
 `;
+
+export const MenuBlock = styled(Menu)`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  justify-content: space-between;
+`;
+
+const { ItemGroup } = MenuBlock
+
+export const ItemGroupBlock = styled(ItemGroup)`
+  height: 100%
+`;
+
+
