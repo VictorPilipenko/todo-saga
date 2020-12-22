3ef9c871bd55f76eb999876e422a47c783bc7920
thejustvic <thejustvic@gmail.com>
2020-12-22 18:38:15 +0200

    default modal

---
+++ src/common/modal/default/index.js
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import { ModalBlock } from './index.styled'
+
+export const useModal = () => {
+  const [isVisible, setIsVisible] = useState(false)
+  const showModal = () => setIsVisible(true)
+  const hideModal = () => setIsVisible(false)
+  const renderModal = ({
+    render,
+    ...props
+  }) => (
+      <ModalBlock
+        destroyOnClose={true}
+        focusTriggerAfterClose={false}
+        visible={isVisible}
+        onCancel={hideModal}
+        {...props}
+      >
+        {typeof render === "function" ? render() : <>Empty Modal</>}
+      </ModalBlock>
+    )
+
+  return {
+    showModal,
+    hideModal,
+    renderModal,
+  }
+}
